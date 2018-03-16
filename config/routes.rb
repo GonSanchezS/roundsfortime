ee96e9e44188c5208a9967448f168b15a226e858
Gonzalo <gonsanchezs@gmail.com>
2018-03-16 18:35:08 +0000

    Finalize landing page structure

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   get 'crossfit-gyms-dublin', to: 'static_pages#dublin'
+  get 'standards', to: 'static_pages#standards'
   get 'test', to: 'static_pages#test'
   get 'crossfit-glossary', to: 'static_pages#glossary'
 end
