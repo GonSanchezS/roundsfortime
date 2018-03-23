d7242b5979324b38f2746aef723b954c5a8b596c
Gonzalo <gonsanchezs@gmail.com>
2018-03-23 18:10:09 +0000

    Add controller and views for boxes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  resources, :boxes, only [:index, :show]
   get 'crossfit-gyms-dublin', to: 'static_pages#dublin'
   get 'standards', to: 'static_pages#standards'
   get 'test', to: 'static_pages#test'
