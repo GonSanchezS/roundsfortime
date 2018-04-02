cc513b720d24ab2432088b681bb263a1736bae84
Gonzalo <gonsanchezs@gmail.com>
2018-04-02 18:16:11 +0000

    Fix seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326165150) do
+ActiveRecord::Schema.define(version: 20180402171742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180326165150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
+    t.string   "currency"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
