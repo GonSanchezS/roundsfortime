1901138890b29d1c123b40c31f5c8537c9890a0a
Gonzalo <gonsanchezs@gmail.com>
2018-04-06 15:39:41 +0000

    Redirect www to naked domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '(*any)', to: redirect(subdomain: ''), via: :all, constraints: {subdomain: 'www'}
   root 'static_pages#index'
   resources :boxes, only: [:index, :show]
   resources :cities, only: [:index, :show]
