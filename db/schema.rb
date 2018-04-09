5fed27af5fd0aa5870c130c9de53d712c3d1e80c
Gonzalo <gonsanchezs@gmail.com>
2018-04-09 17:16:46 +0000

    Add states to cities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402171742) do
+ActiveRecord::Schema.define(version: 20180409171530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180402171742) do
     t.datetime "updated_at", null: false
     t.string   "slug"
     t.string   "currency"
+    t.string   "state"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
