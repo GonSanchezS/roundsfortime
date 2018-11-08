9ad127013cfe84a421e4ec6da9e99133e82dd8a6
Gonzalo <gonsanchezs@gmail.com>
2018-11-08 17:27:56 +0000

    Add styling and more stuff

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,6 +5,7 @@ class CitiesController < ApplicationController
 
   def show
     @city = City.friendly.find(params[:id])
+    @cities = City.all
     @boxes = @city.boxes
     @boxes_count = @boxes.count.to_s
   end
