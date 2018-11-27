cfde4bf130adf4cd95bfa65f60f2cd4bd68c520b
Gonzalo <gonsanchezs@gmail.com>
2018-11-27 21:45:34 +0000

    Add survey to url

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'standards', to: 'static_pages#standards'
   get 'crossfit-glossary', to: 'static_pages#glossary'
   get "/crossfit-prices" => redirect("https://docs.google.com/spreadsheets/d/112UFCh-atAR3rwzGCJitI6RzZ9i0LSP4iH5_sNqSmzE/edit?usp=sharing")
+  get "/submit" => redirect("https://docs.google.com/forms/d/e/1FAIpQLSc9UjVBtOlOxNLHGTGJqLhcxaVF-LBWu2kVxq1dGJrtd7H2-A/viewform?usp=sf_link")
   get 'sitemap', to: 'static_pages#sitemap'
 end
